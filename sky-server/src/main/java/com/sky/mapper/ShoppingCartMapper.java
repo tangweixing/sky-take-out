c4ca100e50f0a74c0f5d1b222cd653099057f354
唐伟星 <3035202256@qq.com>
2025-01-19 10:26:54 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,25 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+/*
+动态条件查询
+ */
+
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+    @Update("update shopping_cart set number=#{number} where id=#{id} ")
+    void updateNumberById(ShoppingCart shoppingCart);
+/*
+插入购物车数据
+ */
+    @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time) VALUES " +
+            "(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{amount},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
