8e2572b14f2657e074e99f2428e71ab8985bba9f
唐伟星 <3035202256@qq.com>
2025-01-19 09:34:20 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -13,6 +13,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealVO.categoryId")//key:setmealCache::100
     public Result insert(@RequestBody SetmealVO setmealVO) {
         log.info("新增套餐:{}", setmealVO);
         setmealService.saveWithDish(setmealVO);
@@ -49,6 +51,7 @@ public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
    */
     @DeleteMapping
     @ApiOperation("删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("套餐批量删除：{}",ids);
         setmealService.deleteBatch(ids);
@@ -59,6 +62,7 @@ public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result updateWithDishes(@RequestBody SetmealVO setmealVO) {
         setmealService.updateWithDishes(setmealVO);
         return Result.success();
@@ -78,6 +82,7 @@ public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("套餐起售停售")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result updateStatus(@PathVariable Integer status,Long id) {
         log.info("套餐起售停售：{},{}", status,id);
         setmealService.updateStatus(status,id);
